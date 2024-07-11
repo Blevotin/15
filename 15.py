cc948d24b0e0b9819fc6bf58726065182fc441a5
Blevotin <dima.dirys@mail.ru>
2024-07-11 23:10:06 +0300

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,14 @@
+def calculate_structure_sum(data_structure):
+    symma = 0
+    if isinstance(data_structure, int):  # проверяем содержит ли параметр функции тип данных int
+        return data_structure
+    if isinstance(data_structure, str):  # проверяем содержит ли параметр функции тип данных str
+        return len(data_structure)
+    if isinstance(data_structure, (list, tuple, set)):  # проверяем содержит ли параметр функции тип данных list, tuple, set
+        for i in data_structure:
+            symma += calculate_structure_sum(i)  # добавляем i-й элемент к сумме при условиях описанных в строке 3 и 5, если не подходит, функция переходит в строку 9 или возвращается в строку 7
+    if isinstance(data_structure, dict):  # проверяем содержит ли параметр функции тип данных dict
+        for key, value in data_structure.items():
+            symma += calculate_structure_sum(key)  # добавляем значения ключа к сумме при условиях описанных в строке 3 и 5
+            symma += calculate_structure_sum(value)  # добавляем значения величины к сумме при условиях описанных в строке 3 и 5
+    return summa
